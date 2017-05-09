deadcb12d6c7eeee2f62c878b7c36312e26df9e9
Olakunle Oladimeji <ola@r-mad.com>
2017-05-09 07:07:27 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,39 @@ restService.use(bodyParser.urlencoded({
     extended: true
 }));
 restService.use(bodyParser.json());
-restService.post('/calculator', function (req, res) {
-    var speech = req.body.result ? req.body.result : "There was a problem with this request";
+restService.post('/mcb', function (req, res) {
+    var action = req.body.result.action ? req.body.result.action : "There was a problem getting the action";
+    switch (action) {
+        case "calculator.questions":
+            var loan_type = req.body.result.parameters.loan_type;
+            var principal = parseFloat(req.body.result.parameters.monthly_payment);
+            var interest_rate = parseFloat(req.body.result.parameters.interest_rate);
+            var payments = parseFloat(req.body.result.parameters.pay_back_over);
+            if (principal == "NaN" || interest_rate == "NaN" || payments == "NaN") {
+                var speech = "There was a problem with this request";
+                var displayText = "There was a problem with this request";
+
+            } else {
+                var x = Math.pow(1 + interest_rate, payments); //Math.pow computes powers
+                var monthly = (principal * x * interest_rate) / (x - 1);
+                if (isFinite(monthly)) {
+                    var total_payment = (monthly * payments).toFixed(2);
+                    var total_interest = ((monthly * payments) - principal).toFixed(2);
+                    var monthly_payment = monthly.toFixed(2);
+                    var entitled_to_amount = 1000;
+                    var speech = "Based on my calculations, you will be able to get a Rs." + entitled_to_amount + ", with an interest of Rs." + total_interest + " over a period of " + payments + "years";
+                } else {
+                    var speech = "There was a problem with this request";
+                }
+            }
+
+            break;
+
+        default:
+            var speech = "There was a problem with this request";
+            var displayText = "There was a problem with this request";
+            break;
+    }
     return res.json({
         speech: speech,
         displayText: speech,
