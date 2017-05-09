48fbd444232c7e1c2af5d21ac01b9a3ade86277b
Olakunle Oladimeji <ola@r-mad.com>
2017-05-09 09:40:15 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 "use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
+const mailer = require("nodemailer");
+let transporter = mailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "kunleoladimeji@gmail.com",
+        pass: "obawalee"
+    }
+})
 const restService = express();
 restService.use(bodyParser.urlencoded({
     extended: true
@@ -8,9 +16,11 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 restService.get("/", function (req, res) {
     return res.send("Hello World!");
-})
+});
 restService.post('/mcb', function (req, res) {
-    var action = req.body.result ? req.body.result.actionType : "There was a problem getting the action";
+    var action = req.body.result ? req.body.result.action : "There was a problem getting the action";
+    var speech = "There was a problem with this request";
+    console.log("Action is " + action);
     switch (action) {
         case "calculator.questions":
             var loan_type = req.body.result.parameters.loan_type;
@@ -18,8 +28,6 @@ restService.post('/mcb', function (req, res) {
             var interest_rate = parseFloat(req.body.result.parameters.interest_rate);
             var payments = parseFloat(req.body.result.parameters.pay_back_over);
             if (principal == "NaN" || interest_rate == "NaN" || payments == "NaN") {
-                var speech = "There was a problem with this request";
-                var displayText = "There was a problem with this request";
 
             } else {
                 var x = Math.pow(1 + interest_rate, payments); //Math.pow computes powers
@@ -31,12 +39,31 @@ restService.post('/mcb', function (req, res) {
                     var entitled_to_amount = 1000;
                     var speech = "Based on my calculations, you will be able to get a Rs." + entitled_to_amount + ", with an interest of Rs." + total_interest + " over a period of " + payments + "years";
                 } else {
-                    var speech = "There was a problem with this request";
+                    speech = "There was a problem with this request";
                 }
             }
 
             break;
-
+        case "loanapplication.questions":
+            var name = req.body.result.parameters.user_name || "Sample User";
+            var loanType = req.body.result.parameters.loan_type;
+            var maritalStatus = req.body.result.parameters.marital_status;
+            var income = req.body.result.parameters.income_amount;
+            var collateral = req.body.result.parameters.collateral;
+            var contact_num = req.body.result.parameters.contact_num;
+            let mailOptions = {
+                from: '"Loan Application ChatBox" <chat@mcb.com>',
+                to: "kunleoladimeji@gmail.com",
+                subject: "New Loan Application",
+                text: "There was an enquiry by " + name + " at " + Date() + "<br />" + "<h1>User Details</h1>" + "Income: " + income + "<br /> Loan Type: " + loanType + "<br /> Collateral: " + collateral + " Contact Number: " + contact_num
+            }
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log("Error!");
+                }
+                console.log("Message %s sent: %s", info.messageId, info.response);
+            });
+            break;
         default:
             var speech = "There was a problem with this request";
             var displayText = "There was a problem with this request";
